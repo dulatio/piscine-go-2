9e4262640ac6a869a763f1bf5d1707d1a8f62ef1
Danonika <student@ALEM-316f6a.alem.school>
2019-10-16 03:31:47 +0600

    WGHAHAHHAHasdsadasd

--- printcomb2.go
+++ printcomb2.go
@@ -5,23 +5,21 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			for k := i; k <= '9'; k++ {
-				for w := j; w <= '9'; w++ {
-					if i == k {
-						w++
-					}
-					z01.PrintRune(i)
-					z01.PrintRune(j)
-					z01.PrintRune(' ')
-					z01.PrintRune(k)
-					z01.PrintRune(w)
-					if i != '9' || j != '8' || k != '9' || w != '9' {
-						z01.PrintRune(',')
+			for k := '0'; k <= '9'; k++ {
+				for w := '0'; w <= '9'; w++ {
+					if i == k && j < w || i < k && j <= w {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
 						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(w)
+						if i != '9' || j != '8' || k != '9' || w != '9' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
 
+						}
 					}
 				}
-
 			}
 		}
 	}
