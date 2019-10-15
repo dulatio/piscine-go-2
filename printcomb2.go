49b5c693edd128dded2eb786b6c0c583bf6df0bb
Danonika <student@ALEM-316f6a.alem.school>
2019-10-16 03:17:31 +0600

    WGHAHAHHAHasdsadasd

---
+++ printcomb2.go
@@ -0,0 +1,21 @@
+package printcomb2
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := i; k <= '9'; k++ {
+				for w := j + 1; w <= '9'; w++ {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(w)
+					z01.PrintRune(',')
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
