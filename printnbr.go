fa46ad3946322374e9a8a46899296d47766caaf2
Danonika <student@ALEM-316f6a.alem.school>
2019-10-16 04:33:02 +0600

    WGHAHAHHAHasdsadasd

--- printnbr.go
+++ printnbr.go
@@ -7,29 +7,30 @@ func PrintNbr(x int) {
 		z01.PrintRune('0')
 	}
 
-		if x < 0 {
-			z01.PrintRune('-')
-			x *= -1
+	if x < 0 {
+		z01.PrintRune('-')
+		x *= -1
+	}
+	y := x
+	ln := 1
+	for {
+		if y == 0 {
+			break
 		}
-    y:=x
-    ln := 1
-    for{
-      if y == 0 {
-        break;}
-        y /= 10
-        ln *= 10;
-    }
-    for{
-      if x == 0 {
-      break;
-      }
-      w := '0'
-      for j:= 0;j <= x / ln; j ++ {
-        w++;
-      }
-      z01.PrintRune(w)
-      x %= ln;
-      ln /= 10;
-    }
+		y /= 10
+		ln *= 10
+	}
+
+	for {
+		if x == 0 {
+			break
+		}
+		w := '0'
+		for j := 0; j <= x/ln; j++ {
+			w++
+		}
+		z01.PrintRune(w)
+		x %= ln
+		ln /= 10
 	}
 }
