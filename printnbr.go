d3f2d52bcb66a207b4ad649604fd57cc13aaae42
Danonika <student@ALEM-316f6a.alem.school>
2019-10-16 04:48:20 +0600

    WGHAHAHHAHasdsadasd

--- printnbr.go
+++ printnbr.go
@@ -2,6 +2,18 @@ package printnbr
 
 import "github.com/01-edu/z01"
 
+func GIVE(y int) {
+	if y == 0 {
+		return
+	}
+	w := '0'
+	for j := 1; j <= y%10; j++ {
+		w++
+	}
+	GIVE(y / 10)
+	z01.PrintRune(w)
+	return
+}
 func PrintNbr(x int) {
 	if x == 0 {
 		z01.PrintRune('0')
@@ -11,26 +23,5 @@ func PrintNbr(x int) {
 		z01.PrintRune('-')
 		x *= -1
 	}
-	y := x
-	ln := 1
-	for {
-		if y == 0 {
-			break
-		}
-		y /= 10
-		ln *= 10
-	}
-
-	for {
-		if x == 0 {
-			break
-		}
-		w := '0'
-		for j := 0; j <= x/ln; j++ {
-			w++
-		}
-		z01.PrintRune(w)
-		x %= ln
-		ln /= 10
-	}
+	GIVE(x)
 }
