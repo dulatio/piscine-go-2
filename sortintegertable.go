aef577a75d08908f352676c48e3241402055f423
Danonika <student@ALEM-316f0f.alem.school>
2019-10-17 14:48:24 +0600

    14:48

---
+++ sortintegertable.go
@@ -0,0 +1,12 @@
+package sortintegertable
+
+func SortIntegerTable(table []int) {
+	for i := 0; i < len(table); i++ {
+		for j := 0; j < len(table); j++ {
+			if table[i] < table[j] {
+				table[i], table[j] = table[j], table[i]
+			}
+		}
+	}
+
+}
