b8d9812ef4f8961b752634488b7840e68ace0095
Danonika <student@ALEM-316f0f.alem.school>
2019-10-17 12:49:45 +0600

    12:49

--- basicatoi.go
+++ basicatoi.go
@@ -3,6 +3,12 @@ package basicatoi
 func BasicAtoi(str string) int {
 	ok := false
 	x := 0
+	ln := 0
+	for _, c := range str {
+		if c == c {
+			ln++
+		}
+	}
 	for _, c := range str {
 		if c != 48 {
 			ok = true
@@ -15,7 +21,7 @@ func BasicAtoi(str string) int {
 			cnt++
 		}
 		x += cnt
-		if c != rune(str[len(str)-1]) {
+		if c != rune(str[ln-1]) {
 			x *= 10
 		}
 	}
